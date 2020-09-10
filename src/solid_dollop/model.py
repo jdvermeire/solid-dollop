032059262bc0eb497ac0195001da003be34b07cc
Jeffrey Vermeire <jdvermeire@gmail.com>
2020-09-10 15:34:54 -0400

    created db model in sqlalchemy

---
+++ src/solid_dollop/model.py
@@ -0,0 +1,107 @@
+# Database model for solid_dollop
+from sqlalchemy import Column, Integer, String, DateTime, Sequence, event, DDL, Index
+from sqlalchemy.sql import func
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# TODO create mixin for crud columns
+
+Base = declarative_base()
+Base.metadata.schema = "drugbank"
+
+# make sure the drugbank schema exists
+def create_schema():
+    event.listen(
+        Base.metadata,
+        "before_create",
+        DDL("CREATE SCHEMA IF NOT EXISTS drugbank")
+    )
+
+
+class Drugs(Base):
+    """
+
+    """
+    __tablename__ = 'drugs'
+
+    id = Column(Integer, Sequence('drug_id_seq'), primary_key=True)
+    drugbank_id = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    smiles = Column(String)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
+
+
+class Genes(Base):
+    """
+
+    """
+    __tablename__ = 'genes'
+
+    id = Column(Integer, Sequence("gene_id_seq"), primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
+
+
+class Actions(Base):
+    """
+
+    """
+    __tablename__ = 'actions'
+
+    id = Column(Integer, Sequence("action_id_seq"), primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
+
+
+class AltSources(Base):
+    """
+
+    """
+    __tablename__ = 'alt_sources'
+
+    id = Column(Integer, Sequence("alt_source_id_seq"), primary_key=True),
+    drugbank_id = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
+
+
+class DrugTargets(Base):
+    """
+
+    """
+    __tablename__ = 'drug_targets'
+
+    id = Column(Integer, Sequence("drug_target_id_seq"), primary_key=True)
+    drug_id = Column(Integer, nullable=False, index=True)
+    gene_id = Column(Integer, nullable=False, index=True)
+    action_ids = Column(Array(Integer), nullable=False)
+    name = Column(String, nullable=False)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
+
+
+class DrugAltIds(Base):
+    """
+
+    """
+    __tablename__ = 'drug_alt_ids'
+
+    id = Column(Integer, Sequence("drug_alt_id_id_seq"), primary_key=True)
+    drug_id = Column(Integer, nullable=False, index=True),
+    alt_source_id = Column(Integer, nullable=False, index=True)
+    alt_id = Column(String, nullable=False)
+    alt_id_url = Column(String, nullable=False)
+    created_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    modified_ts = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    deleted_ts = Column(DateTime(timezone=True))
--- tests/solid_dollop/test_drugbank.py
+++ tests/solid_dollop/test_drugbank.py
@@ -6,6 +6,10 @@ from solid_dollop import DrugBank
 def test_init():
     pass
 
+def test_parse_meta():
+    soup = BeautifulSoup('<meta name="dc.title" content="happy" /><meta name="description" content="the happy path" />', features="lxml")
+    assert DrugBank._parse_meta(soup) == ("happy", "the happy path")
+
 def test_parse_container():
     def test_fun(container):
         return container.string
@@ -30,18 +34,58 @@ def test_parse_targets():
         '<div id="targets"><div class="bond-list"><div class="bond card"></div><div class="bond card"></div><div class="bond card"></div></div></div>',
         features="lxml"
     )
+
+    out = BeautifulSoup('<div class="bond card"></div>', features="lxml").div
+
     targets_container = soup.find(id="targets")
     assert DrugBank._parse_targets(targets_container) == [
-        '<div class="bond card"></div>',
-        '<div class="bond card"></div>',
-        '<div class="bond card"></div>',
+        out,
+        out,
+        out,
     ]
 
 def test_parse_target_gene_name():
-    pass
+    soup = BeautifulSoup(
+        '<div class="bond card"><div class="card-body"><div class="row"><div><dl><dt id="gene-name">Gene Name</dt><dd>HAPPY</dd></dl></div></div></div></div>',
+        features="lxml"
+    )
+
+    tgn_container = soup.find(id="gene-name")
+
+    assert DrugBank._parse_target_gene_name(tgn_container) == 'HAPPY'
 
 def test_parse_target_actions():
-    pass
+    soup = BeautifulSoup(
+        '<dt class="col-md-5 col-sm-6" id="actions">Actions</dt><dd class="col-md-7 col-sm-6"><div class="badge badge-pill badge-action">Inhibitor</div></dd>',
+        features="lxml"
+    )
+
+    action_container = soup.find(id="actions")
+
+    assert DrugBank._parse_target_actions(action_container) == ['Inhibitor']
 
 def test_parse_alt_ids():
-    pass
+    soup = BeautifulSoup(
+        '<dt class="col-xl-2 col-md-3 col-sm-4" id="external-links">External Links</dt><dd class="col-xl-10 col-md-9 col-sm-8"><dl class="inner-dl"><dt class="col-md-4 col-sm-5" id="human-metabolome-database">Human Metabolome Database</dt><dd class="col-md-8 col-sm-7"><a target="_blank" rel="noopener" href="http://www.hmdb.ca/metabolites/HMDB0001859">HMDB0001859</a></dd><dt class="col-md-4 col-sm-5" id="wikipedia">Wikipedia</dt><dd class="col-md-8 col-sm-7"><a target="_blank" rel="noopener" href="http://en.wikipedia.org/wiki/Paracetamol">Paracetamol</a></dd></dl></dd>',
+        features="lxml"
+    )
+
+    alt_ids_container = soup.find(id="external-links")
+
+    assert DrugBank._parse_alt_ids(alt_ids_container) == [
+        {
+            "alt_id_source_id": "human-metabolome-database",
+            "alt_id_source_name": "Human Metabolome Database",
+            "alt_id": "HMDB0001859",
+            "alt_id_url": "http://www.hmdb.ca/metabolites/HMDB0001859",
+        },
+        {
+            "alt_id_source_id": "wikipedia",
+            "alt_id_source_name": "Wikipedia",
+            "alt_id": "Paracetamol",
+            "alt_id_url": "http://en.wikipedia.org/wiki/Paracetamol",
+        },
+    ]
+
+
+# TODO add mocks to conftest.py in order to test things that require requests
